71212cd142d667cb1a1a8713221ab5e9a2046aca
soheila <soheilalotfi@ymail.com>
2019-08-10 00:26:40 -0400

    add another prompt to ask user about the item

--- greatbay.js
+++ greatbay.js
@@ -20,8 +20,46 @@ inquirer
   .then(function(answers) {
     console.log(answers.Your_choice);
     if (answers.Your_choice === "post an item") {
+      itemInformation();
     }
   });
+
+function itemInformation() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "item_name",
+        message: "what is the item's name?"
+      },
+      {
+        type: "input",
+        name: "item_category",
+        message: "what is the item's category?"
+      }
+    ])
+    .then(function(userInput) {
+      var itemName = userInput.item_name;
+      var itemCategory = userInput.item_category;
+      postItem(itemName, itemCategory);
+    });
+}
+function postItem(itemName, itemCategory) {
+  connection.connect(function(err) {
+    if (err) throw err;
+    connection.query(
+      "INSERT INTO auctions SET ?",
+      {
+        item_name: itemName,
+        category: itemCategory
+      },
+      function(err, res) {
+        if (err) throw err;
+        connection.end();
+      }
+    );
+  });
+}
 
 //If the user selects "POST AN ITEM" they are prompted for an assortment of information regarding the item
 //and then that information is added to the database so that others can bid on it
