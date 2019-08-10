4b52aac5d25a44551f5b216bd98675782abc6c07
soheila <soheilalotfi@ymail.com>
2019-08-10 00:03:41 -0400

    add mysql package

--- greatbay.js
+++ greatbay.js
@@ -1,13 +1,27 @@
 var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  passwoed: "Leila@1357",
+  database: "greatBay_DB"
+});
 
 inquirer
   .prompt([
     {
       type: "list",
-      name: "Your choice",
+      name: "Your_choice",
       choices: ["post an item", "bid on an item"]
     }
   ])
   .then(function(answers) {
-    console.log(answers);
+    console.log(answers.Your_choice);
+    if (answers.Your_choice === "post an item") {
+    }
   });
+
+//If the user selects "POST AN ITEM" they are prompted for an assortment of information regarding the item
+//and then that information is added to the database so that others can bid on it
